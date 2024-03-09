cae4e149a0be6fa55eed76463ad988dec9af3c69
minhle28 <leminh9844@gmail.com>
2024-03-09 00:30:32 -0500

    Building course-detail page

--- client/src/pages/courses-details/index.jsx
+++ client/src/pages/courses-details/index.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
 import './courses-details.css';
 import { Link } from 'react-router-dom';
-
+import { coursesData, comments } from '../dummyData/'; 
 
 export const CoursesDetails = () => {
+
+    const course = coursesData[0];
+
     return (
         <div id="courses_details">
             <div className='sub-header-course-detail'>
                 <Link to="/courses">
-                    <i class="fa-solid fa-arrow-left"></i>
+                    <i className="fa-solid fa-arrow-left"></i>
                 </Link>
                 <button className="action-button">Give a Review</button>
             </div>
-            <div className='coursesdetails-container' >
+            <div className='coursesdetails-container'>
                 <div className='container'>
                     <div id="title_container" className='container'>
                         <h2>Course Name</h2>
@@ -22,19 +25,42 @@ export const CoursesDetails = () => {
                             <p>Instructor: John Doe</p>
                         </div>
                     </div>
-                    <br />
-
+                    <br /><br />
                     <div className='main-review-container'>
                         <div className='student-review'>
                             <h4>Student Reviews:</h4>
+                            {/* Loop over comments and render each one */}
+                            {comments.map((comment, index) => (
+                                <div key={index} className="container mt-4">
+                                    <div className="d-flex row">
+                                        <div className="col-md-12">
+                                            <div className="d-flex flex-column comment-section">
+                                                <div className="bg-white p-4">
+                                                    <div className="d-flex flex-row user-info">
+                                                        <img className="rounded-circle" src="https://i.imgur.com/RpzrMR2.jpg" alt='avatar' width="40" />
+                                                        <div className="d-flex flex-column justify-content-start ml-2">
+                                                            <span className="d-block font-weight-bold name">{comment.name}</span>
+                                                            <span className="date text-black-50">{comment.date}</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className="mt-3">
+                                                        <p className="comment-text">{comment.comment}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                         <div className='course-description'>
                             <h6>Course Description:</h6>
-
+                            <p>{course.description}</p>
                         </div>
                     </div>
                 </div>
             </div>
+            <br/><br/>
         </div>
     );
 }
--- client/src/pages/dummyData/index.jsx
+++ client/src/pages/dummyData/index.jsx
@@ -1,14 +1,47 @@
 export const coursesData = [
-    { id: 1, courseCode: 'Course 1', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 2, courseCode: 'Course 2', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 3, courseCode: 'Course 3', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 4, courseCode: 'Course 4', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 5, courseCode: 'Course 5', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 6, courseCode: 'Course 6', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 7, courseCode: 'Course 7', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 8, courseCode: 'Course 8', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 9, courseCode: 'Course 9', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 10, courseCode: 'Course 10', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 11, courseCode: 'Course 11', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 12, courseCode: 'Course 12', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 1, courseCode: 'Course 1', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 2, courseCode: 'Course 2', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 3, courseCode: 'Course 3', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 4, courseCode: 'Course 4', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 5, courseCode: 'Course 5', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 6, courseCode: 'Course 6', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 7, courseCode: 'Course 7', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 8, courseCode: 'Course 8', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 9, courseCode: 'Course 9', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 10, courseCode: 'Course 10', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 11, courseCode: 'Course 11', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { id: 12, courseCode: 'Course 12', instructor: 'Prof.', semester: 'Spring 2023', description: 'Quote 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+];
+
+export const comments = [
+    {
+        name: 'Marry Andrews',
+        date: 'Shared publicly - Jan 2020',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        name: 'Minh Le',
+        date: 'Shared publicly - Jan 2018',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        name: 'Marry Andrews',
+        date: 'Shared publicly - Jan 2020',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        name: 'Minh Le',
+        date: 'Shared publicly - Jan 2018',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        name: 'Marry Andrews',
+        date: 'Shared publicly - Jan 2020',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        name: 'Minh Le',
+        date: 'Shared publicly - Jan 2018',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
 ];
